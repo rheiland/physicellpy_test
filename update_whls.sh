6f1d07e1b8c3e0225957b9fdb155e7e38dacf415
Randy Heiland <randy.heiland@gmail.com>
2026-09-23 08:01:37 -0400

    clean

--- update_whls.sh
+++
@@ -1,3 +0,0 @@
-gh release download --clobber v0.1.2 -R rheiland/physicellpy -p physicellpy-0.1.0-cp312-cp312-macosx_14_0_arm64.whl
-gh release download --clobber v0.1.2 -R rheiland/physicellpy -p physicellpy-0.1.0-cp313-cp313-manylinux_2_17_x86_64.manylinux2014_x86_64.whl
-gh release download --clobber v0.1.2 -R rheiland/physicellpy -p physicellpy-0.1.0-cp314-cp314-macosx_14_0_arm64.whl
